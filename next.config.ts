5b5ced727069667417824b4c1be3b28dd3282c7b
harrisstef8 <harrisstef8@gmail.com>
2026-01-18 16:50:31 +0200

    Add env in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  images: {
-    unoptimized: true,
-  },
+  images: { unoptimized: true },
   trailingSlash: true,
+  env: {
+    NEXT_PUBLIC_BASE_PATH: "", 
+  },
 };
 
 export default nextConfig;
