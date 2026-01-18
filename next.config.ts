49ca6ec7a811eee495569d53fadbbd192c25df77
harrisstef8 <harrisstef8@gmail.com>
2026-01-18 16:43:04 +0200

    Fix Next.js config for custom domain

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,11 @@
 import type { NextConfig } from "next";
 
-const repo = "my-portfolio"; // ⬅️ ΒΑΛΕ ΕΔΩ ΑΚΡΙΒΩΣ ΤΟ ΟΝΟΜΑ ΤΟΥ REPO ΣΟΥ
-
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: `/${repo}`,
-  assetPrefix: `/${repo}/`,
   trailingSlash: true,
-  env: {
-    NEXT_PUBLIC_BASE_PATH: `/${repo}`,
-  },
 };
 
 export default nextConfig;
